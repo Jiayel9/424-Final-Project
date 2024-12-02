0d39e82da7da6d722e142526a0d61846e9d92404
Kevin L <kevin.liu3@mail.mcgill.ca>
2024-12-01 21:17:59 -0500

    test

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -131,7 +131,8 @@ class StudentAgent(Agent):
 
     # 6, 8, 10, 12 accounts for board sizes
     board_sizes = [6, 8, 10, 12]
-    depth = board_sizes.index(board.shape[0])
+    depths = [6,4,3,2]
+    depth = depths.index(board.shape[0]) #Selects initial search depth based on board size
     best_move = None 
     best_score = float('-inf')
 
